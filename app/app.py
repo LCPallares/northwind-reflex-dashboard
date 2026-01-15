af5aeeb565734685ec400ca3d2fa12238127ae3a
LCPallares <poncho6257@gmail.com>
2026-01-15 16:26:07 -0500

    fix; importar etado de analisis

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from app.pages.products import products_page
 from app.pages.customers import customers_page
 from app.pages.analytics import analytics_page
 from app.states.orders_state import OrdersState
+from app.states.analytics_state import AnalyticsState
 from app.components.sidebar import sidebar
 from app.components.main_content import main_content
 
